58eace0693f236991432a2a5c0bc0c091eb029be
Teja Konduri <tejakondury@gmail.com>
2017-07-03 14:06:20 -0500

    Update therm_cond_2.m

--- therm_cond_2.m
+++ therm_cond_2.m
@@ -59,19 +59,46 @@ b1=(sb/((sp^2-sb^2)+mk*(1-sp^2))+(sp-sb)/(sp^2+mk*(1-sp^2))+(1-sp)/mk)^(-1);
 b0=(temp/theta_0)^(1/2);
 
 km=kb*b1*b0;
+
+
+
+# material Graphite-Epoxy UD composite laminate
+
+cond=[0.01386,0.00262,0.00205,0.00173,0.00173];
+temp_cond=[10,524,565,815,3316]
+temp_cond=temp_cond+273;
+
+density=[1.5967,1.5967,1.1497,1.1497];
+density=density*1000;
+temp_dens=[10,523,524,3316]
+temp_dens=temp_dens+273
+
+sp=[1.2250,2.0569,5.0095,4.9046,1.5939,2.5098];
+temp_sp=[10,330,357,496,524,3316]
+
+
+hold on
 figure
 ezplot(km,[theta_0 1073])
 title('Variation of Km with temp')
 xlabel('temp in Kelvin')
 ylabel('Km W/mK')
+scatter(temp_cond,cond)
+%xlabel('Temp in Kelvin')
+%ylabel('Thermal conductivity W/mK')
+%title('variation of thermal cond Chen')
 
 
-figure
-exp=kb*b0;
-ezplot(exp, [theta_0 1073])
-title('variation of Kb with temp')
-xlabel('Temp in Kelvin')
-ylabel('Kb W/mK')
+
+
+
+
+%figure
+%exp=kb*b0;
+%ezplot(exp, [theta_0 1073])
+%title('variation of Kb with temp')
+%xlabel('Temp in Kelvin')
+%ylabel('Kb W/mK')
 %zz=int(km,a,b);
 %an=zz;
 %ezplot(an, [theta_0 1073])
